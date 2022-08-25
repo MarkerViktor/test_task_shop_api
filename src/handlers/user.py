9efc47e6e5e8ce660a9dcd56ff2e4b442b1e47f7
Victor <MarkerViktor@outlook.com>
2022-08-25 18:23:09 +0600

    Change response serialization to pydantic. Add default values for query params limit and offset.

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -1,26 +1,27 @@
 import uuid
-from dataclasses import asdict
 
 import pydantic
 import sanic
 from sanic import exceptions
 
-from src.entities import UserType
-from src.handlers.utils import require, PydanticQuery, Auth
+from src.entities import User, UserType
+from src.handlers.utils import require, PydanticQuery, Auth, pydantic_response
 from src.services import user
 
 
 api_user = sanic.Blueprint('api_user')
 
 
+class UsersList(pydantic.BaseModel):
+    users: list[User]
+
 @api_user.get('/users/')
 @require(Auth(allowed=[UserType.admin]))
 async def get_users(r: sanic.Request) -> sanic.HTTPResponse:
     user_service: user.UserService = r.app.ctx.user_service
-    limit, offset = r.args.get('limit'), r.args.get('offset')
+    limit, offset = r.args.get('limit', 10), r.args.get('offset', 0)
     users = await user_service.get_users(limit, offset)
-    users_as_dicts = list(asdict(u) for u in users)
-    return sanic.json(users_as_dicts)
+    return pydantic_response(UsersList(users=users))
 
 
 @api_user.post('/user/<user_id:int>/<action:(de)?activate>', name='deactivate user')
